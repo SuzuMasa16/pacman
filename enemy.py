815a3702e78973d5c49ebcfee088b8e1aee1816e
mine555 <youyangf513@gmail.com>
2024-11-12 17:31:54 +0900

    player,food,enemy変更

--- enemy.py
+++ enemy.py
@@ -1,10 +1,11 @@
 from item import Item
+import random
 
 
-class Player(Item):
-    """プレイヤークラス
-    Itemを継承して作成したプレイヤークラス.
-    入力から移動方向を受け取って移動しようとする方向を計算するメソッドと
+class enemy(Item):
+    """ エネミークラス
+    Itemを継承して作成したエネミークラス.
+    入力を受け取ってランダムな方向に移動したときの」計算するメソッドと
     マップから移動して良いと許可が出た時に呼び出される座標を更新するメソッド
     を追加.
 
@@ -16,22 +17,19 @@ class Player(Item):
         self.next_y(int) : 次の時刻でのy座標
         self.status(bool) : アイテムの状態（Trueなら存在する、Falseなら存在しない消滅した）
     """
-    
     def __init__(self, x, y) -> None:
         pass
 
         super().__init__(x, y)
-        self.icon = "😶"
+        self.icon = "E"
 
-    def get_next_pos(self, dir: tuple[int, int]) -> tuple[int, int]:
+    def get_next_pos(self) -> tuple[int, int]:
         """
-        入力から移動方向を受け取って移動しようとする方向を計算して次の座標を返すメソッド
-        引数にキー入力から受け取った次に移動したい方向をとり,
-        現在のプレイヤーの座標から次に移動したい座標を戻り値として出力する.
+        入力を受け取りランダムな方向に移動した方向を計算して次の座標を返すメソッド
+        現在のプレイヤーの座標から次に移動したい座標を[num1,num2]で決めて戻り値として出力する.
 
         Args:
-            dir (tuple[int, int]): キー入力から受け取った次に移動したい方向.
-            (例:右に1マス移動したかったら(1,0)を受け取る)
+            
 
         Returns:
             tuple[int, int]: 次に移動したい座標(例:入力が(1,0)で現在のプレイヤーの座標が
@@ -46,6 +44,10 @@ class Player(Item):
             (2, 4)
 
         """
+
+        num1=random.randint(-1,1)
+        num2=random.randint(-1,1)
+        dir=(num1,num2)
         self.next_x = self.now_x + dir[0]
         self.next_y = self.now_y + dir[1]
         return (self.next_x, self.next_y)
