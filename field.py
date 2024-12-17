abda5a7a00fafe10eb95b1fafdc75b15257b91dd
SuzuMasa16 <h160116masato@gmail.com>
2024-12-17 16:51:13 +0900

    fieldを更新

--- field.py
+++ field.py
@@ -1,4 +1,9 @@
 from player import Player
+from item import Item
+from enemy import Enemy
+from food import Food
+from block import Block
+
 
 class Field:
     """Fieldクラス
@@ -8,14 +13,16 @@ class Field:
 
     Attributes:
         players (list[Player]): プレイヤーのリスト
+        f_size (int): フィールドのサイズ
         enemies (list[Enemy]): 敵のリスト
         foods (list[Food]]): アイテムのリスト
         blocks (list[Block]): アイテムのリスト
         field (list[list[str]]): フィールドの情報
-        f_size (int): フィールドのサイズ
     """
 
-    def __init__(self, field_size: int, players: list[Player]) -> None:
+    def __init__(self, field_size: int, players: list[Player], 
+                 enemies: list[Enemy], foods: list[Food],
+                 blocks: list[Block]) -> None:
         """"
         初期化関数
 
@@ -28,6 +35,10 @@ class Field:
         self.players = players
         self.field = [[" "for _ in range(field_size)]
                       for _ in range(field_size)]
+        self.players = players
+        self.enemies = enemies
+        self.foods = foods
+        self.blocks = blocks
         self.update_field()
         pass
 
@@ -49,10 +60,49 @@ class Field:
             for j in range(self.field_size):
                 self.field[i][j] = " "
 
+        for food in self.foods:
+            self.field[food.now_y][food.now_x] = food.icon
+        for enemy in self.enemies:
+            self.field[enemy.now_y][enemy.now_x] = enemy.icon
+        for block in self.blocks:
+            self.field[block.now_y][block.now_x] = block.icon
         for player in self.players:
             self.field[player.next_x][player.next_y] = player.icon
 
         return self.field
+
+    def check_bump(  # お手本のパクリ
+            self,
+            target: Item,
+            items: list[Item]) -> Item | None:
+        """
+        2つのアイテムの位置が重なっているか判定する関数
+
+        Args:
+            target (Item): アイテム1
+            items (list[Item]): アイテムのリスト2
+
+        Returns:
+            Item | None: 重なっているアイテムがあればそのアイテム、なければNone
+
+        Examples:
+            >>> p = Item(0, 0)
+            >>> e = Item(1, 1)
+            >>> field = Field([p], [e], [], [])
+            >>> p.next_x = 1
+            >>> r = field.check_bump(p, [e])
+            >>> r is None
+            True
+            >>> p.next_y = 1
+            >>> r = field.check_bump(p, [e])
+            >>> r is e
+            True
+        """
+        # 衝突判定を行う処理を記述
+        for item in items:
+            if item.next_x == target.next_x and item.next_y == target.next_y:
+                return item
+        return None
 
     def display_field(self) -> None:
         """
